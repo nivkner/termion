dafa78e5a18c4f01bac0a5ae763d77d23afa5f92
Niv Kaminer <nivkner@zoho.com>
2017-12-28 18:59:50 +0200

    implement get_tty

--- src/sys/windows/tty.rs
+++ src/sys/windows/tty.rs
@@ -1,10 +1,10 @@
-use std::os::windows::io::AsRawHandle;
-use std::{io, fs};
+use std::os::windows::io::{AsRawHandle, FromRawHandle};
+use std::os::windows::ffi::OsStrExt;
+use std::{io, fs, ptr, ffi};
 use super::winapi::um::handleapi::INVALID_HANDLE_VALUE;
-use super::winapi::um::winuser::GetWindowThreadProcessId;
-use super::winapi::um::processenv::GetStdHandle;
-use super::winapi::um::consoleapi::GetConsoleMode;
-use super::winapi::um::processthreadsapi::GetCurrentProcessId;
+use super::winapi::um::winnt::{GENERIC_READ, GENERIC_WRITE, FILE_SHARE_READ, FILE_SHARE_WRITE};
+use super::winapi::um::fileapi::{CreateFile2, OPEN_EXISTING};
+use super::winapi::um::consoleapi::{GetConsoleMode};
 
 pub fn is_tty<T: AsRawHandle>(stream: &T) -> bool {
     let std_handle = stream.as_raw_handle();
@@ -20,5 +20,19 @@ pub fn is_tty<T: AsRawHandle>(stream: &T) -> bool {
 ///
 /// This allows for getting stdio representing _only_ the TTY, and not other streams.
 pub fn get_tty() -> io::Result<fs::File> {
-    unimplemented!();
+    let handle = unsafe {CreateFile2( ffi::OsStr::new("CONIN$")
+                                      .encode_wide()
+                                      .chain(Some(0).into_iter())
+                                      .collect::<Vec<_>>()
+                                      .as_ptr(),
+                                      GENERIC_READ | GENERIC_WRITE,
+                                      FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                      OPEN_EXISTING,
+                                      ptr::null_mut())};
+    if handle == INVALID_HANDLE_VALUE {
+        Err(io::Error::last_os_error())
+    } else {
+        let file = unsafe { fs::File::from_raw_handle(handle) };
+        Ok(file)
+    }
 }
