1d569e7e8fa4eb8db758cf13327445e7879f94c4
Niv Kaminer <nivkner@zoho.com>
2017-12-28 22:37:45 +0200

    attempt size

--- src/sys/windows/size.rs
+++ src/sys/windows/size.rs
@@ -1,6 +1,17 @@
-use std::io;
+use std::{io, ptr};
+use std::os::windows::io::AsRawHandle;
+use sys::tty::get_write_tty;
+
+use super::winapi::um::wincon::GetConsoleScreenBufferInfo;
 
 /// Get the size of the terminal.
 pub fn terminal_size() -> io::Result<(u16, u16)> {
-    unimplemented!()
+    let console_pointer = ptr::null_mut();
+	if unsafe { GetConsoleScreenBufferInfo(io::stdout().as_raw_handle(), console_pointer) != 0 } {
+        unsafe {console_pointer.as_ref()}
+			.map(|info| (info.dwSize.X as u16, info.dwSize.Y as u16))
+			.ok_or(io::Error::last_os_error())
+    } else {
+       Err(io::Error::last_os_error())
+    }
 }
