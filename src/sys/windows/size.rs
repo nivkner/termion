9bd626e6063feb231d0b8fbac95f35c273f2b037
Niv Kaminer <nivkner@zoho.com>
2017-12-28 23:11:26 +0200

    fix size

--- src/sys/windows/size.rs
+++ src/sys/windows/size.rs
@@ -1,17 +1,18 @@
-use std::{io, ptr};
+use std::{io, mem};
 use std::os::windows::io::AsRawHandle;
-use sys::tty::get_write_tty;
-
 use super::winapi::um::wincon::GetConsoleScreenBufferInfo;
 
 /// Get the size of the terminal.
 pub fn terminal_size() -> io::Result<(u16, u16)> {
-    let console_pointer = ptr::null_mut();
-	if unsafe { GetConsoleScreenBufferInfo(io::stdout().as_raw_handle(), console_pointer) != 0 } {
-        unsafe {console_pointer.as_ref()}
-			.map(|info| (info.dwSize.X as u16, info.dwSize.Y as u16))
-			.ok_or(io::Error::last_os_error())
-    } else {
-       Err(io::Error::last_os_error())
+    let stdout_handle = io::stdout().as_raw_handle();
+
+    unsafe {
+        let mut csbi = mem::zeroed();
+        if GetConsoleScreenBufferInfo(stdout_handle, &mut csbi) != 0 {
+            Ok(((csbi.srWindow.Right - csbi.srWindow.Left + 1) as u16,
+                (csbi.srWindow.Bottom - csbi.srWindow.Top + 1) as u16))
+        } else {
+            Err(io::Error::new(io::ErrorKind::Other, format!("Unable to get the terminal size: {}", io::Error::last_os_error())))
+        }
     }
 }
